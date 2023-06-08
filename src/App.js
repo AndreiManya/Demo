8c81d70f236ba53145c202210b7de67a5b76fe71
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-06-08 17:02:43 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from './components/main/index.js'
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/main/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from './logo/logo.js'
+
+ const Main = () => { 
+
+
+  return (
+    <>
+      <Logo/>
+    </>
+  );
+}
+
+
+export default Main;
